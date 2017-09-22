887768b91bb65a7886d130da76939c4bb4706d5f
trupti <hawaldartrupti@gmail.com>
2017-09-22 10:02:04 -0700

    Adding API structure

---
+++ BillingAPI/src/main/java/com/billingapi/demo/BillingApiApplication.java
@@ -0,0 +1,12 @@
+package com.billingapi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingApiApplication.class, args);
+	}
+}
